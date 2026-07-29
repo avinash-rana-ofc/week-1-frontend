e2402d55d82b5da2c1e1dd9dbd5fb4f1170d6d92
Avinash <avinashrana55@yahoo.com>
2026-07-29 18:16:55 +0530

    feat : Added reset button and customized input field to set counter

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+//add reset button
 function App() {
   const [counter, setCounter] = useState(0);
+  const [input, setInput] = useState(0);
   return (
-    <div style={{textAlign: 'center', marginTop : '50px'}}>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello, Fullstack developer!</h1>
       <h2>Count: {counter}</h2>
       <button onClick={() => setCounter(counter + 1)}>Increase</button>
       <button onClick={() => setCounter(counter - 1)}>Decrease</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+      <div>
+        <label>Customized Input</label>
+        <input
+          type="number"
+          value={counter}
+          onChange={(e) => setCounter(Number(e.target.value))}
+        />
+      </div>
     </div>
   );
 }
