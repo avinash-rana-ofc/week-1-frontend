8f81c70f754c0fa7209c222be3453cf9f0e69362
Avinash <avinashrana55@yahoo.com>
2026-08-10 18:24:01 +0530

    feat: added delete functionality and persists todos in localstorage

--- todo-app/src/Todo.js
+++ todo-app/src/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Todo = () => {
   const [tasks, setTasks] = useState([]);
@@ -7,6 +7,22 @@ const Todo = () => {
   const handleAddTodo = () => {
     setTasks([...tasks, input]);
   };
+
+  const handleDeleteTodo = (index) => {
+    tasks.splice(index, 1);
+    setTasks([...tasks]);
+  };
+
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (savedTasks.length !== 0) {
+      setTasks(savedTasks);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div>
@@ -17,10 +33,21 @@ const Todo = () => {
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <button onClick={handleAddTodo}>Add Todo</button>
-        
-        <ul style={{backgroundColor : 'red'}}>
-          {tasks.map((task, index) => <li key={index}>{task}</li>)}
+
+        <button onClick={handleAddTodo}> Add Todo</button>
+
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}{" "}
+              <button
+                className="delete-btn"
+                onClick={() => handleDeleteTodo(index)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
